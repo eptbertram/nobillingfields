b1883f3ffd0b7243a217168d98819b7ace651e3d
scardinius <scardinius@chords.pl>
2019-06-26 15:27:26 +0200

    Update civix

--- nobillingfields.php
+++ nobillingfields.php
@@ -14,8 +14,6 @@ function nobillingfields_civicrm_config(&$config) {
 /**
  * Implements hook_civicrm_xmlMenu().
  *
- * @param array $files
- *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
  */
 function nobillingfields_civicrm_xmlMenu(&$files) {
@@ -61,13 +59,6 @@ function nobillingfields_civicrm_disable() {
 /**
  * Implements hook_civicrm_upgrade().
  *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
  */
 function nobillingfields_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
@@ -91,8 +82,6 @@ function nobillingfields_civicrm_managed(&$entities) {
  *
  * Generate a list of case-types.
  *
- * @param array $caseTypes
- *
  * Note: This hook only runs in CiviCRM 4.4+.
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
@@ -112,7 +101,7 @@ function nobillingfields_civicrm_caseTypes(&$caseTypes) {
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
  */
 function nobillingfields_civicrm_angularModules(&$angularModules) {
-_nobillingfields_civix_civicrm_angularModules($angularModules);
+  _nobillingfields_civix_civicrm_angularModules($angularModules);
 }
 
 /**
@@ -154,6 +143,7 @@ function nobillingfields_civicrm_buildForm($formName, &$form) {
     }
   }
 }
+
 
 /**
  * @param $formName
